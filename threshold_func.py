34c11aca05cbdce7244f2f6c7926a5a4cc719c45
Benjamin Deck <49119525+bdeck8317@users.noreply.github.com>
2022-01-18 11:37:12 -0500

    first commit of docs and code

---
+++ threshold_func.py
@@ -0,0 +1,11 @@
+def threshold_net(path_to_csv)
+"""This function creates a number of csv files with different thresholds for functional MRI data. These thresholds have been preset using 
+the guidelines based on van den Heuvel et al., 2017----see: https://www.sciencedirect.com/science/article/pii/S105381191730109X?via%3Dihub"""
+
+
+
+
+
+import numpy as np
+import pandas as pd
+
